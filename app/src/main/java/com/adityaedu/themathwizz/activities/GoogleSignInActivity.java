e23138b0414f205b2c0877ec63a0651359a35834
Preetham <preetham.hk@gmail.com>
2018-01-31 15:01:44 +0530

    commit

---
+++ app/src/main/java/com/adityaedu/themathwizz/activities/GoogleSignInActivity.java
@@ -0,0 +1,10 @@
+package com.adityaedu.themathwizz.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by preet on 1/23/2018.
+ */
+
+public class GoogleSignInActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/adityaedu/themathwizz/server/StarterApplication.java
@@ -0,0 +1,10 @@
+package com.adityaedu.themathwizz.server;
+
+import android.app.Application;
+
+/**
+ * Created by preet on 1/24/2018.
+ */
+
+public class StarterApplication extends Application {
+}
